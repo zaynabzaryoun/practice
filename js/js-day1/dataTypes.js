e3dc9c8b97c5e9ce39d9fa721dd4efa381fd0ec4
zaynabzaryoun <anaab@gmail.com>
2024-11-12 23:54:50 +0330

    first

---
+++ js/js-day1/dataTypes.js
@@ -0,0 +1,19 @@
+const name = "zaa" //string
+const age = 22 //numbers 
+const isOlder = false // boolean
+const book = null
+let language;
+let id = Symbol('id'); //symbol
+let largerNum = 374378n;
+let n1 = 1; 
+// let answer = n1 + largerNum //error
+let answer = BigInt(n1) + largerNum
+// console.log(answer);
+
+console.log(language);  //undefined
+
+console.log(x); // not defined
+
+
+
+
