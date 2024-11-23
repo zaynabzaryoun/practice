97cf2065d9213118e5b701fe9ae61ef7b1337765
zaynabzaryoun <anaab@gmail.com>
2024-11-23 17:40:25 +0330

    review

--- js/js-2/index.js
+++ js/js-2/index.js
@@ -136,16 +136,72 @@ for (let score of scores) {
 
 // console.log(person);
 
-function sum(a, b, c) {
-  return a + b + c;
-}
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
 
-const numbers = [1, 2, 3];
+// const numbers = [1, 2, 3];
 
-console.log(sum(...numbers));
+// console.log(sum(...numbers));
 
-const array1 = [1, 2, 3];
-const array2 = [4, 5, 6];
-const cimbined = [...array1, ...array2]
-console.log(cimbined);
+// const array1 = [1, 2, 3];
+// const array2 = [4, 5, 6];
+// const cimbined = [...array1, ...array2]
+// console.log(cimbined);
+
+// const colors = ["red", "green", "blue", "purple", "orange"];
+// const [first, second] = colors
+// console.log(first);
+// console.log(second);
+// const [, second, third] = colors
+// console.log(second, third);
+
+// const [color1, color2, ,...rest] = colors
+// console.log(rest);
+
+
+// const person = { name: "Zaynab", age: 25 };
+// let { name: firstName, age: personAge } = person
+
+// console.log(firstName, personAge);
+// firstName = "sara"
+
+// let { firstName, lastName, middleName } = person;
+// console.log(middleName); // undefined
+
+// let { name, age: personAge = 30, lname: lastName = "zaryoun" } = person
+// console.log(lastName);
+// console.log(person);
+
+// let employee = {
+//   id: 1001,
+//   name: {
+//       firstName: 'John',
+//       lastName: 'Doe'
+//   }
+// }
+
+// let { name: { firstName } } = employee
+
+
+
+// const car = {
+//   brand: "Toyota",
+//   model: "Corolla",
+//   year: 2022,
+//   isElectric: false,
+//   color: "blue"
+// };
+// console.log(Object.keys(car));
+// console.log(Object.values(car));
+
+
+// function logger(name= "zaynab") {
+//   console.log("hello", name);
+  
+// }
+
+// logger("ali")
+
+
 
