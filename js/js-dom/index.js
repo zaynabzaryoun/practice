5c773593344df3c2d7319d9fc4e7370de23ce789
zaynabzaryoun <33anaab@gmail.com>
2024-12-04 23:46:32 +0330

    dom

--- js/js-dom/index.js
+++ js/js-dom/index.js
@@ -1,13 +1,38 @@
-const button = document.getElementById("btn")
-const output = document.getElementById("output")
+// const button = document.getElementById("btn")
+// const output = document.getElementById("output")
+// const radios = document.getElementsByName("rate")
 
-// console.log(radios);
+// button.addEventListener("click", () => {
+//     radios.forEach(radio => {
+//         if (radio.checked) {
+//             output.innerHTML = `you clicked ${radio.value}`
+//         }
+//     })
+// })
 
-button.addEventListener("click", () => {
-    let rates = document.getElementsByName("rate")
-    rates.forEach(rate => {
-        if (rate.checked) {
-            output.innerText = rate.value
-        }
-    })
-})
+// const h2s = document.getElementsByTagName("h2")
+// const button = document.getElementById("btnCount")
+// console.log(h2s);
+// const h2sArray = Array.of(...h2s)
+// console.log(h2sArray);
+
+// button.addEventListener("click", () => {
+//     alert(`number of h2 is: ${h2sArray.length}`)
+// })
+
+// const ul = document.getElementById("menu")
+// const lis = ul.getElementsByClassName("item")
+// console.log(lis);
+// const lisArray = Array.of(...lis)
+// const data = lisArray.map(item => item.textContent)
+// console.log(data);
+
+
+
+const elements = document.getElementsByClassName("secondary")
+console.log(elements);
+
+const data = Array.of(...elements).map(e => e.textContent)
+console.log(data);
+
+
