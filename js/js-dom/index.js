acf88dec36f8d0387fc29bc0fc5e030a380d4a4e
zaynabzaryoun <33anaab@gmail.com>
2024-12-06 15:40:13 +0330

    part1

--- js/js-dom/index.js
+++ js/js-dom/index.js
@@ -29,10 +29,49 @@
 
 
 
-const elements = document.getElementsByClassName("secondary")
-console.log(elements);
+// const element = document.querySelectorAll("li")
+// console.log(element);
+// let prant = element.parentNode
+// console.log(prant);
 
-const data = Array.of(...elements).map(e => e.textContent)
-console.log(data);
+// const data = Array.of(...elements).map(e => e.textContent)
+// console.log(data);
 
+
+// let note = document.querySelector(".note");
+// console.log(note);
+// console.log(note.parentNode);
+
+
+// let node = document.querySelector(".current")
+// // let nextSibling = node.nextSibling
+// let nextSiblingElment = node.nextElementSibling
+// // console.log(nextSibling);
+// console.log(nextSiblingElment);
+// let previos = node.previousElementSibling
+
+// while (previos) {
+//     console.log(previos);
+//     previos = previos.previousElementSibling
+    
+// }
+
+
+// while (nextSiblingElment) {
+//     console.log(nextSiblingElment);
+//     // nextSiblingElment = nextSiblingElment.nextElementSibling
+//     node = node.nextElementSibling
+// }
+
+
+
+// let content = document.getElementById("menu")
+// let firstChile = content.firstChild
+// console.log(firstChile);
+// let children1 = content.childNodes
+// console.log(children1);
+// let children2 = content.children
+// console.log(children2);
+
+ 
 
