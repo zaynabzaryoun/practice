a734d5371aeff11ddc2c88bfdf391379b866d358
zaynabzaryoun <33anaab@gmail.com>
2024-12-12 22:05:31 +0330

    icons and articles fixed

--- css-projects/2_start-page/app.js
+++ css-projects/2_start-page/app.js
@@ -1,39 +1,6 @@
 const barIcon = document.querySelector(".fa-bars")
 
-// function showBars() {
-//      if (window.innerWidth < 450) {
-//     barIcon.classList.remove("hidden")
-// } else {
-//     barIcon.classList.add("hidden")
-//     // list.classList.add("hidden"); 
-// }
-// }
-
-// showBars()
-
 const list = document.querySelector(".list")
-
-// window.addEventListener("resize", showBars)
-// window.addEventListener("resize", showMenu)
-
-
-// function showMenu() {
-//     if (window.innerWidth < 450) {
-//         list.classList.remove("hidden")
-//     } else {
-//         list.classList.add("hidden")
-//     }
-// }
-
-// showMenu()
-// barIcon.addEventListener("click", () => {
-//     if (window.innerWidth < 450 && list.classList.contains("hidden")) {
-//         list.classList.remove("hidden")
-//     } else {
-//         list.classList.add("hidden")
-//     }
-// })
-
 
 function showBarAndList() {
     if (window.innerWidth < 450) {
