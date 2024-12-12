3b06e13dfde77da226b7266e9851e475fc6549b1
zaynabzaryoun <33anaab@gmail.com>
2024-12-12 16:05:13 +0330

    fixed resposive menu

--- css-projects/2_start-page/app.js
+++ css-projects/2_start-page/app.js
@@ -1,36 +1,60 @@
 const barIcon = document.querySelector(".fa-bars")
-console.log(barIcon);
 
-function showBars() {
-     if (window.innerWidth < 450) {
-    barIcon.classList.remove("hidden")
-} else {
-    barIcon.classList.add("hidden")
-    // list.classList.add("hidden"); 
-}
-}
+// function showBars() {
+//      if (window.innerWidth < 450) {
+//     barIcon.classList.remove("hidden")
+// } else {
+//     barIcon.classList.add("hidden")
+//     // list.classList.add("hidden"); 
+// }
+// }
 
-showBars()
+// showBars()
 
 const list = document.querySelector(".list")
 
-window.addEventListener("resize", showBars)
-window.addEventListener("resize", showMenu)
+// window.addEventListener("resize", showBars)
+// window.addEventListener("resize", showMenu)
 
 
-function showMenu() {
+// function showMenu() {
+//     if (window.innerWidth < 450) {
+//         list.classList.remove("hidden")
+//     } else {
+//         list.classList.add("hidden")
+//     }
+// }
+
+// showMenu()
+// barIcon.addEventListener("click", () => {
+//     if (window.innerWidth < 450 && list.classList.contains("hidden")) {
+//         list.classList.remove("hidden")
+//     } else {
+//         list.classList.add("hidden")
+//     }
+// })
+
+
+function showBarAndList() {
     if (window.innerWidth < 450) {
-        list.classList.remove("hidden")
+        barIcon.classList.remove("hidden")
+        list.classList.add("hidden")
     } else {
+        barIcon.classList.add("hidden")  
         list.classList.add("hidden")
     }
 }
+showBarAndList()
 
-// showMenu()
 barIcon.addEventListener("click", () => {
-    if (window.innerWidth < 450 && list.classList.contains("hidden")) {
-        list.classList.remove("hidden")
+    list.classList.toggle("hidden")
+})
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth < 460) {
+        barIcon.classList.remove("hidden")
     } else {
+        barIcon.classList.add("hidden")
         list.classList.add("hidden")
     }
 })
