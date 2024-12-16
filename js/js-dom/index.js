7dc21df87477b1356008a7e9b8090a80ef378765
zaynabzaryoun <33anaab@gmail.com>
2024-12-16 14:48:05 +0330

    from

--- js/js-dom/index.js
+++ js/js-dom/index.js
@@ -555,11 +555,172 @@ const el = document.querySelector(".ele")
 
 // })();
 
-const form = document.querySelector("#form")
+// const form = document.querySelector("#form")
 
-console.log(form.elements["email"].value)
+// console.log(form.elements["email"].value)
 
-form.addEventListener("event", () => {
+// form.addEventListener("event", () => {
     
-})
+// })
+
+
+// form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     const name = form.elements["name"]
+//     console.log(name);
+//     const email = form.elements["email"]
+//     console.log(email);
+//     let isValid = true
+
+
+//     if (name.value.trim() === "") {
+//         alert(NAME_REQUIRED);
+//         isValid = false
+//     }
+
+//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     if (!email.value.match(emailRegex)) {
+//         alert(EMAIL_INVALID)
+//         isValid = false
+//     }
+    
+//     if (isValid) {
+//         alert("form submitted successfully")
+//     }
+
+// })
+
+// function showError(input, message) {
+//     let errorElement = input.nextElementSibling
+//     errorElement.textContent = message
+//     input.classList.add("error")
+
+//  }
+
+// function showSuccess(input) {
+//     const errorElemnent = input.nextElementSibling
+//     errorElemnent.textContent = ""
+//     input.classList.remove("error")
+// }
+
+// function showMessage(input, message, type) {
+//     const msg = input.parentNode.querySelector("small")
+//     msg.innerText = message
+
+//     input.className = type ? "success" : "error"
+//     return type
+// }
+
+// function showError(input, message) {
+//     return showMessage(input, message, false)
+// }
+
+// function showSuccess() {
+//     return showMessage(input, "", true)
+// }
+
+// function hasValue(input, message) {
+//     if (input.value.trim() === "") {
+//         return showError(input, message)
+//     }
+
+//     return showSuccess(input)
+// }
+
+// function validateEmail(input, requiredMsg, invalidMsg) {
+//     if (!hasValue(input, requiredMsg)) {
+//         return false
+        
+//     }
+
+//     const emailRegex =
+// 		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     const email = input.value.trim()
+
+// }
+
+//     if (!emailRegex.text(email)) {
+//         return showError(input, invalidMsg)
+// }
+// return true
+
+    
+// const form = document.querySelector("#signup");
+
+// const NAME_REQUIRED = "Please enter your name";
+// const EMAIL_REQUIRED = "Please enter your email";
+// const EMAIL_INVALID = "Please enter a correct email address format";
+
+
+// form.addEventListener("submit", function (event) {
+//     event.preventDefault()
+
+//     let nameValid = hasValue(form.elements["name"], NAME_REQUIRED)
+//     let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID)
+
+//     if (nameValid && emailValid) {
+//         alert("Demo only. No form was posted.")
+//     }
+// })
+
+
+// show a message with a type of the input
+function showMessage(input, message, type) {
+	// get the small element and set the message
+	const msg = input.parentNode.querySelector("small");
+	msg.innerText = message;
+	// update the class for the input
+	input.className = type ? "success" : "error";
+	return type;
+}
+
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
+
+function hasValue(input, message) {
+	if (input.value.trim() === "") {
+		return showError(input, message);
+	}
+	return showSuccess(input);
+}
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+	// check if the value is not empty
+	if (!hasValue(input, requiredMsg)) {
+		return false;
+	}
+	// validate email format
+	const emailRegex =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	const email = input.value.trim();
+	if (!emailRegex.test(email)) {
+		return showError(input, invalidMsg);
+	}
+	return true;
+}
+
+const form = document.querySelector("#signup");
+
+const NAME_REQUIRED = "Please enter your name";
+const EMAIL_REQUIRED = "Please enter your email";
+const EMAIL_INVALID = "Please enter a correct email address format";
+
+form.addEventListener("submit", function (event) {
+	// stop form submission
+	event.preventDefault();
+
+	// validate the form
+	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
+	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+	// if valid, submit the form.
+	if (nameValid && emailValid) {
+		alert("Demo only. No form was posted.");
+	}
+});
 
