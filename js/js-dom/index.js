af22cd5a12792f35c8d0fc573ecd493e728c1a95
zaynabzaryoun <33anaab@gmail.com>
2024-12-21 23:16:52 +0330

    checkbox

--- js/js-dom/index.js
+++ js/js-dom/index.js
@@ -665,62 +665,92 @@ const el = document.querySelector(".ele")
 
 
 // show a message with a type of the input
-function showMessage(input, message, type) {
-	// get the small element and set the message
-	const msg = input.parentNode.querySelector("small");
-	msg.innerText = message;
-	// update the class for the input
-	input.className = type ? "success" : "error";
-	return type;
-}
+// function showMessage(input, message, type) {
+// 	// get the small element and set the message
+// 	const msg = input.parentNode.querySelector("small");
+// 	msg.innerText = message;
+// 	// update the class for the input
+// 	input.className = type ? "success" : "error";
+// 	return type;
+// }
 
-function showError(input, message) {
-	return showMessage(input, message, false);
-}
+// function showError(input, message) {
+// 	return showMessage(input, message, false);
+// }
 
-function showSuccess(input) {
-	return showMessage(input, "", true);
-}
+// function showSuccess(input) {
+// 	return showMessage(input, "", true);
+// }
 
-function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
-	}
-	return showSuccess(input);
-}
+// function hasValue(input, message) {
+// 	if (input.value.trim() === "") {
+// 		return showError(input, message);
+// 	}
+// 	return showSuccess(input);
+// }
 
-function validateEmail(input, requiredMsg, invalidMsg) {
-	// check if the value is not empty
-	if (!hasValue(input, requiredMsg)) {
-		return false;
-	}
-	// validate email format
-	const emailRegex =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// function validateEmail(input, requiredMsg, invalidMsg) {
+// 	// check if the value is not empty
+// 	if (!hasValue(input, requiredMsg)) {
+// 		return false;
+// 	}
+// 	// validate email format
+// 	const emailRegex =
+// 		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-	const email = input.value.trim();
-	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg);
-	}
-	return true;
-}
+// 	const email = input.value.trim();
+// 	if (!emailRegex.test(email)) {
+// 		return showError(input, invalidMsg);
+// 	}
+// 	return true;
+// }
 
-const form = document.querySelector("#signup");
+// const form = document.querySelector("#signup");
 
-const NAME_REQUIRED = "Please enter your name";
-const EMAIL_REQUIRED = "Please enter your email";
-const EMAIL_INVALID = "Please enter a correct email address format";
+// const NAME_REQUIRED = "Please enter your name";
+// const EMAIL_REQUIRED = "Please enter your email";
+// const EMAIL_INVALID = "Please enter a correct email address format";
 
-form.addEventListener("submit", function (event) {
-	// stop form submission
-	event.preventDefault();
+// form.addEventListener("submit", function (event) {
+// 	// stop form submission
+// 	event.preventDefault();
 
-	// validate the form
-	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
-	// if valid, submit the form.
-	if (nameValid && emailValid) {
-		alert("Demo only. No form was posted.");
-	}
-});
+// 	// validate the form
+// 	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
+// 	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+// 	// if valid, submit the form.
+// 	if (nameValid && emailValid) {
+// 		alert("Demo only. No form was posted.");
+// 	}
+// });
 
+
+///////////////////////////////////////////////////////////
+// const checkbox = document.getElementById("input: checked")
+// console.log(checkbox);
+
+
+// const checkbox = document.getElementById("checkbox")
+
+// console.log(checkbox.value);
+
+// const btn = document.querySelector("button")
+// btn.onclick = () => {
+// 	console.log(checkbox.value);
+	
+// }
+
+
+const btn = document.querySelector("#btn")
+btn.addEventListener("click", (e) => {
+	const checkboxs = document.querySelectorAll("input:checked")
+	console.log(checkboxs);
+	let value = []
+	checkboxs.forEach(e => {
+		value.push(e.value);
+	})
+	alert(value)
+	console.log(value);
+	
+	
+})
