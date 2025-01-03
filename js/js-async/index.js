319459db88e9bab31f2ca686afaebc7316bfb8d4
zaynabzaryoun <33anaab@gmail.com>
2025-01-03 23:59:11 +0330

    promise example

--- js/js-async/index.js
+++ js/js-async/index.js
@@ -85,3 +85,107 @@
 //     console.log(msg);
     
 // })
+
+// let p = new Promise(function (resovle, reject) {
+//     let a = 1 + 1 // performe a task
+//     if (a === 2) {
+//         resovle("success")
+//     } else {
+//         reject("error")
+//     }
+// })
+
+// p.then((msg) => {
+//     console.log("then", msg);
+    
+// }).catch((err) => {
+//     console.log("catch", err);
+    
+// })
+
+// function getUser() {
+//     return [
+//         { username: 'john', email: 'john@test.com' },
+//         { username: 'jane', email: 'jane@test.com' },
+//     ];
+// }
+
+// function findUser(usernamee) {
+//     return new Promise(function(res, rej){
+//         const users = getUser();
+//         const user = users.find(u => {
+//           return  u.username === usernamee
+//         })
+//         if (user) {
+//             res(`${user.username}`)
+//         } else {
+//             rej("user not found")
+//         }
+//     })
+// }
+
+// findUser("john")
+//     .then((msg) => {
+//         console.log("user found: ", msg);
+        
+//     }).catch((err) => {
+//         console.log("error", err);
+        
+//     })
+
+
+
+let stocks = {
+    fruits: ["strawberry", "grapes", "banana", "apple"],
+    liquids: ["water", "ice"],
+    holders: ["cone", "cup", "stick"],
+    toppings: ["chocolate", "peanuts"]
+}
+
+let is_shop_open = true
+
+let order = (time, work) => {
+    return new Promise((resolve, reject) => {
+        if (is_shop_open) {
+            setTimeout(() => {
+                resolve(work())
+            }, time)
+        } else {
+            reject(console.log("our shop is closed"))
+        }
+    })
+}
+
+order(2000, () => console.log(`${stocks.fruits[0]} was selected`))
+
+    .then(() => {
+        return order(0o0, () =>console.log("production has started"))   
+    })
+
+    .then(() => {
+        return order(2000, () =>console.log("fruits were shops"))
+    })
+
+    .then(() => {
+        return order(2000, ()=> console.log(`${stocks.liquids[0]} and ${stocks.liquids[1]} was selected`)
+        )
+    })
+
+    .then(() => {
+        return order(1000, ()=> console.log("start the machine"))
+    })
+
+    .then(() => {
+        return order(2000, ()=> console.log(`ice cream placed on ${stocks.holders[0]}`)
+        )
+    })
+    
+    .then(() => {
+        return order(3000, ()=> console.log(`${stocks.toppings[0]} was selected`)
+        )
+    })
+
+    .then(() => {
+        return order(2000, ()=> console.log("ice is sereved")
+        )
+    })
