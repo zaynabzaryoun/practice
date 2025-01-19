361931df850ba716dc40aea4a534bf15d5bd1193
zaynabzaryoun <33anaab@gmail.com>
2025-01-20 00:21:50 +0330

    im done with this api!

--- js/js-small-project/eng-dictionary/index.js
+++ js/js-small-project/eng-dictionary/index.js
@@ -8,21 +8,15 @@ const definitionDiv = document.querySelector(".definition");
 const ukEle = document.querySelector(".uk");
 const usEle = document.querySelector(".us");
 const partOfSpeechEle = document.querySelector(".part-of-speech");
-const uUk = document.querySelector(".u-uk");
-const uUs = document.querySelector(".u-us");
-const kUk = document.querySelector(".k-uk");
-const kUs = document.querySelector(".k-us");
 
 let searchedWord = "";
 const ukSound = new Audio("");
 const usSound = new Audio("");
 
 btn.addEventListener("click", getTranslation);
-console.log(input.value);
 
 input.addEventListener("keyup", (e) => {
   searchedWord = e.target.value;
-  console.log(searchedWord);
 });
 
 async function getTranslation() {
@@ -33,30 +27,18 @@ async function getTranslation() {
     );
     if (!response.ok) {
       console.log(response.statusText);
-      }
-      
-      
-      const data = await response.json();
-      console.log(data[0].meanings);
-    
-    if (!data|| data.length === 0) {
-      wordEle.textContent = "Word Not Found!";
     }
+      
+    const data = await response.json();
 
     wordEle.textContent = data[0].word;
-
-    console.log(data[0].phonetics.find((e) => e.audio.includes("-uk")).audio);
-
+    
     ukSound.src = data[0].phonetics.find((e) => e.audio.includes("-uk")).audio;
     usSound.src = data[0].phonetics.find((e) => e.audio.includes("-us")).audio;
-    console.log(ukSound.src);
-    ukSound.src ? (uUk.style.color = "red") : (uUk.style.color = "blue");
 
     phoneticEle.textContent = data[0].phonetics[1].text;
 
-    console.log(data[0].meanings[0].definitions[0].definition);
-      data[0].meanings[0].definitions.map((definition) => {
-    console.log(definition.definition);
+    data[0].meanings[0].definitions.map((definition) => {
       const p = document.createElement("p");
       p.textContent = definition.definition;
       definitionDiv.append(p);
@@ -70,11 +52,9 @@ async function getTranslation() {
 getTranslation();
 ukEle.addEventListener("click", () => {
   ukSound.play();
-  console.log("play uk");
 });
 usEle.addEventListener("click", () => {
   usSound.play();
-  console.log("play us");
 });
 
 function clear() {
