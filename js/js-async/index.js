576fb6f39944ec69d0f62ed7534e582e7f8ce1bf
zaynabzaryoun <33anaab@gmail.com>
2025-01-31 23:48:53 +0330

    projects

--- js/js-async/index.js
+++ js/js-async/index.js
@@ -318,3 +318,62 @@
 // }
 
 // getPosts()
+
+
+// const getPosts = async () => {
+//     try {
+//         const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+//         console.log(response);
+        
+//         const data = await response.json()
+//         console.log(data);
+        
+
+//     } catch(err) {
+//         console.log(err);
+        
+//     }
+// }
+
+// getPosts()
+
+
+// const obj = { name: "sara", age: 25 };
+// const json = JSON.stringify(obj)
+// console.log(typeof json);
+
+
+
+
+// http://latentflip.com/loupe/?code=JC5vbignYnV0dG9uJywgJ2NsaWNrJywgZnVuY3Rpb24gb25DbGljaygpIHsKICAgIHNldFRpbWVvdXQoZnVuY3Rpb24gdGltZXIoKSB7CiAgICAgICAgY29uc29sZS5sb2coJ1lvdSBjbGlja2VkIHRoZSBidXR0b24hJyk7ICAgIAogICAgfSwgMjAwMCk7Cn0pOwoKY29uc29sZS5sb2coIkhpISIpOwoKc2V0VGltZW91dChmdW5jdGlvbiB0aW1lb3V0KCkgewogICAgY29uc29sZS5sb2coIkNsaWNrIHRoZSBidXR0b24hIik7Cn0sIDUwMDApOwoKY29uc29sZS5sb2coIldlbGNvbWUgdG8gbG91cGUuIik7!!!PGJ1dHRvbj5DbGljayBtZSE8L2J1dHRvbj4%3D
+
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Introduction
+
+
+// function sayHi() {
+//   return "hello";
+// }
+
+// async function sing() {
+//   return "la la la la"; // promise
+// }
+
+// console.log(sayHi());
+
+// console.log(sing());
+
+// sing().then(m => console.log(m)
+// ).catch(e=>console.log(e)
+// )
+
+
+async function sign() {
+    return "la la la"
+
+    throw "oh no"
+}
+
+
+
+sign().then(e =>console.log(e)
+).catch(e=>console.log(e))
