9188bca939ba467b85d1a6993be53b3a9f3b7714
zaynabzaryoun <33anaab@gmail.com>
2025-02-10 23:51:10 +0330

    sidebar

--- js/js-small-project/sidebar/app.js
+++ js/js-small-project/sidebar/app.js
@@ -1,5 +1,6 @@
 const barIcon = document.querySelector(".bar-container")
 const nav = document.querySelector("nav")
+const closeIcon = document.querySelector(".fa-times")
 
 
 function displayBar() {
@@ -14,6 +15,11 @@ function handleBarClick() {
     nav.classList.toggle("hidden")
 }
 
+function handleCloseClick() {
+    nav.classList.add("hidden")
+}
+
 window.addEventListener("DOMContentLoaded", displayBar)
 window.addEventListener("resize", displayBar)
 barIcon.addEventListener("click", handleBarClick)
+closeIcon.addEventListener("click", handleCloseClick)
