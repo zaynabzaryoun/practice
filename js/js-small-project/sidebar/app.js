1410d33848b44a747ac572d632f178fac19029fc
zaynabzaryoun <33anaab@gmail.com>
2025-02-11 22:59:13 +0330

    done

--- js/js-small-project/sidebar/app.js
+++ js/js-small-project/sidebar/app.js
@@ -6,17 +6,21 @@ const closeIcon = document.querySelector(".fa-times")
 function displayBar() {
     if (window.innerWidth > 500) {
         barIcon.classList.add("hidden")
+        nav.classList.remove("hidden")
     } else {
         barIcon.classList.remove("hidden")
+        nav.classList.add("hidden")
     }
 } 
 
 function handleBarClick() {
     nav.classList.toggle("hidden")
+    barIcon.classList.toggle("hidden")
 }
 
 function handleCloseClick() {
     nav.classList.add("hidden")
+    barIcon.classList.remove("hidden")
 }
 
 window.addEventListener("DOMContentLoaded", displayBar)
