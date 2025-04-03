f902b240ebf3e6b3f4060b91b998e56b6d8ad649
zaynabzaryoun <33anaab@gmail.com>
2025-04-03 23:59:21 +0330

    js

---
+++ js/jsy/main.js
@@ -0,0 +1,40 @@
+// console.log("hello from main.js");
+
+// const name = "zzzz vvvv";
+// const language = "js";
+// const favoriteColor = `blue`;
+
+// const s = 0
+// const pi = 3.14
+ 
+// const boolean = false
+// const boolean2 = true
+
+// let home; //undefined
+// // console.log(home);
+
+
+// const c1 = undefined
+// // console.log(c1);
+
+// const data = null
+
+
+// //obj literal
+// const person = {
+//     fname: "x",
+//     lname: "f",
+//     age: 30
+// }
+
+// // console.log(person.fname)
+
+// const n = [1, 3, 5, 7, 9]
+// console.log(n[2]);
+
+// let a = 3
+// a = "g"
+// a = true
+
+
+
