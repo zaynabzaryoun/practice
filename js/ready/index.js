99244b9053cf9e6431ef7f41ed1a11f640a3a508
zaynabzaryoun <33anaab@gmail.com>
2025-06-03 23:58:39 +0330

    js

---
+++ js/ready/index.js
@@ -0,0 +1,62 @@
+// console.log("hello");
+//  1. Variables & Data Types
+
+// let a = 5;
+// let b = "5"
+// console.log(a == b);
+// console.log(a===b);
+
+// Q2: Identify the type
+// console.log(typeof null); // ?
+// console.log(typeof NaN);  // ?
+
+// Q3: Convert '123' to a number
+// let s = "123"
+// let n = Number(s)
+// console.log(typeof n);
+
+// Q4: Convert 456 to a string
+// let n = 456
+// let s = n.toString()
+// console.log(typeof s);
+
+// ✅ 2. Operators
+// Q1: What will this log?
+// let x = true;
+// let y = false;
+// console.log(x && y); // ?
+// console.log(x || y); // ?
+
+// Q2: Use a ternary to check if a number is even or odd
+// (33 % 2 === 0) ? console.log("even") : console.log("odd");
+
+// ✅ 3. Conditionals & Loops
+// Q1: Write a function to find the factorial of a number
+
+// function factorialFunc(n) {
+//     let p = 1;
+//     if (n === 0) {
+//         return 1;
+//     }
+//     else {
+//         for (let i = n; i >= 1; i--){
+//         p *= i;
+//     }
+//         return p;
+//     }
+// }
+
+// console.log(factorialFunc(3));
+
+
+
+// Q2: Write a function to reverse a string
+
+
+
+
+// Q3: Write a function to check if a number is prime
+
+
+
+
